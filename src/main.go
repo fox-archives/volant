e8bb1b51851e8d96d749f24cb55a587b5463686d
repl.it user <replituser@example.com>
2020-08-10 16:41:04 +0000

    compiler CLI

--- src/main.go
+++ src/main.go
@@ -7,18 +7,36 @@ import (
 	"io/ioutil"
 	. "parser"
 	"time"
+    "os"
+    "os/exec"
+    "flag"
 )
 
 func main() {
-	Code, _ := ioutil.ReadFile("test.vo")
-	Default, _ := ioutil.ReadFile("default.c")
-
-	fmt.Println(string(Default))
+    fileName := flag.String("compile","","file to be compiled")
+    flag.Parse()
+    if *fileName == "" {
+        fmt.Println("file name not given")
+        os.Exit(1)
+    }
+    path, _ := os.Getwd()
+	Code, _ := ioutil.ReadFile(path + "/" + *fileName)
+    f,_ := os.Create(path+"/test.c")
+    f.Write(Default)
 
 	t1 := time.Now()
-	code := string(CompileFile(AnalyzeFile(ParseFile(&Lexer{Buffer: Code}))))
-	t2 := time.Now()
+	_,err:=f.Write(CompileFile(AnalyzeFile(ParseFile(&Lexer{Buffer: Code}))))
+    t2 := time.Now()
+    cmd := exec.Command("clang","test.c", "-pthread" ,"-fblocks" ,"-lBlocksRuntime" ,"-lgc", "-isystem/home/runner/.apt/usr/include/")
 
-	print("Compiling took " + t2.Sub(t1).String() + ".\n\n")
-	fmt.Println(code)
+    if err:=cmd.Run(); err!= nil{
+        exec.Command("install-pkg","libgc-dev","libblocksruntime-dev").Run()
+    }
+    out,err:=exec.Command("clang","test.c", "-pthread" ,"-fblocks" ,"-lBlocksRuntime" ,"-lgc", "-isystem/home/runner/.apt/usr/include/").CombinedOutput()
+    if err!= nil{fmt.Println(string(out))}
+	t3 := time.Now()
+    exec.Command("rm", "test.c").Run()
+	fmt.Println("Compiling to C took " + t2.Sub(t1).String() + ".\n\n")
+    fmt.Println("Compiling from C took " + t3.Sub(t2).String() + ".\n\n")
+	
 }
